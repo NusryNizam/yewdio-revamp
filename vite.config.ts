6d3e4e7e14b3d6c6ddfaa817a8bb4f372f909c99
Nusry Nizam <nusrynizam@gmail.com>
2024-07-01 23:32:50 +0530

    fix: config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,47 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate", manifest: false })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Yewdio",
+        short_name: "Yewdio",
+        description: "Listen to music on-the-go.",
+        theme_color: "#ff8515",
+        background_color: "#101010",
+        categories: ["entertainment", "music"],
+        display: "standalone",
+        id: "/",
+        scope: "/",
+        start_url: "https://yewdio.netlify.app",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
